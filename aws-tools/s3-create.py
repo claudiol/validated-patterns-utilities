fef85367ac291ffaf73aa0b827786ed494abffbe
Lester Claudio <claudiol@redhat.com>
2022-01-06 10:30:54 -0700

    Adding S3 create and delete sources

---
+++ aws-tools/s3-create.py
@@ -0,0 +1,168 @@
+#!/usr/bin/python
+
+import boto3
+import botocore
+from botocore.exceptions import ClientError
+import sys
+import getopt
+import os
+import json
+
+regions=['us-west-1', 'us-west-2', 'us-east-1', 'us-east-2']
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+
+
+# Remove 1st argument from the
+# list of command line arguments
+argumentList = sys.argv[1:]
+
+# Options
+options = "hpb:r:"
+
+# Long options
+long_options = ["help", "bucket", "region", "public"]
+
+def usage():
+  msg = "Usage: python " + sys.argv[0] + "\n"  \
+        "Options: \n" \
+        " -b or --bucket \n" \
+        "    S3 bucket name to create \n" \
+        "    Example: -b my-bucket \n" \
+        " -r or --region \n" \
+        "    AWS region for bucket \n" \
+        "    Example: -r us-west-1 \n" \
+        " -h or --help \n" \
+        "    Usage message"
+  print(msg)
+  exit()
+
+def getSTSClient ():
+    try:
+        sts = boto3.client('sts')
+        return sts
+    except ClientError:
+        print ("Could not create STS client")
+        raise
+    
+def getSTSSessionToken( sts ):
+    try:
+        token = sts.get_session_token()
+        sessionToken = token['Credentials']['SessionToken']
+        return sessionToken
+    except ClientError:
+        print("Could not retrieve STS Session Token")
+        raise
+    
+def getS3Client(region):
+    endpoint_url = 'https://s3.' + region + '.amazonaws.com' 
+
+    try:
+        s3 = boto3.client('s3',
+                          endpoint_url = endpoint_url,
+                          aws_access_key_id = aws_access_key_id,
+                          aws_secret_access_key = aws_secret_access_key,
+                          region_name = region,
+                          config=botocore.client.Config(signature_version = 's3'))
+        return s3
+    except ClientError:
+        print("Could not retrieve S3 client")
+        raise
+
+def getSNSClient(region):
+    endpoint_url = 'https://s3.' + region + '.amazonaws.com' 
+
+    sts = getSTSClient()
+    sessionToken = getSTSSessionToken(sts)
+    try:
+        sns = boto3.client('sns',
+                           endpoint_url = endpoint_url,
+                           aws_access_key_id = aws_access_key_id,
+                           aws_secret_access_key= aws_secret_access_key,
+                           aws_session_token=sessionToken,
+                           region_name=region,
+                           config=botocore.client.Config(signature_version = 's3'))
+        return sns
+    except ClientError:
+        print ("Could not retrieve SNS client")
+        raise
+    
+def create_bucket(s3, bucket_name, region, public=None):
+    try:
+        result = s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': region})
+        if public:
+          arn="arn:aws:s3:::" + bucket_name + "/*"
+          policy={
+            "Version": "2012-10-17",
+            "Statement": [
+              {
+                "Sid": "AllowALLStatement1",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": "s3:*",
+                "Resource": arn
+              }
+            ]
+          }
+          # Convert the policy from JSON dict to string
+          bucket_policy = json.dumps(policy)
+          getS3Client(region).put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy)
+          
+        return result
+    except ClientError as e:
+        print("Could not create bucket [" + bucket_name + "]")
+        print(e)
+        raise
+  
+def main():
+    bucket=""
+    region=""
+    public=None
+
+    try:
+        # Parsing argument
+        arguments, values = getopt.getopt(argumentList, options, long_options)
+
+        if len(arguments) == 0:
+            usage()
+            
+        # checking each argument
+        for currentArgument, currentValue in arguments:            
+            if currentArgument in ("-h", "--help"):
+                usage()
+                break
+            elif currentArgument in ("-b", "--bucket"):
+                print ("Creating S3 Bucket: ", currentValue)
+                bucket=currentValue
+            elif currentArgument in ("-r", "--region"):
+                print ("Target S3 Bucket Region: ", currentValue)
+                region=currentValue
+            elif currentArgument in ("-p", "--public"):
+                print ("WARNING: S3 Bucket will be made public.")
+                public=True
+
+    except getopt.error as err:
+        # output error, and return with an error code
+        print (str(err))
+        usage()
+        exit()
+
+    if region == "":
+      print ("Missing required region")
+      usage()
+      
+    if bucket == "":
+      print ("Missing required bucket name")
+      usage()
+    
+    #s3 = getS3Client(region)
+    s3 = boto3.resource('s3')
+    sns = getSNSClient(region)
+        
+    result = create_bucket(s3, bucket, region, public)
+
+    if result:
+      print (result)
+      
+if __name__ == "__main__":
+    main()
